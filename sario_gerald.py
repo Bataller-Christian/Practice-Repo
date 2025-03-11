a5afc87deaf4e06e6dc698619e13f9ffb6b1b0e2
teves-gwen <tevesgwenn.sch@gmail.com>
2025-03-11 14:08:47 +0800

    Fix merge conflict

--- sario_gerald.py
+++ sario_gerald.py
@@ -4,9 +4,7 @@ print("Gender: Male")
 print("Email: sariogrld02@gmail.com")
 
 Welcome to the team!!! - Gerald :P
-<<<<<<< HEAD
 
 Welcome to the team! - Gwenn
 =======
 COMMENT COMMENT!
->>>>>>> 285eb203498754bba629887792d3f03accf115ad
