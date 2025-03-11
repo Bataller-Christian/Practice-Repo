a5d04d474e6b0f868816a9be6c011bf14678263e
sario-gerald <sariogrld02@gmail.com>
2025-03-11 15:35:58 +0800

    add comm

--- teves_gwenntwo.py
+++ teves_gwenntwo.py
@@ -3,9 +3,10 @@ Welcome to the team! - Gwen
 Welcome ulet - Gwen2
 
 Welcome To The Team - Bataller
-Welcome - Gwen3
-Welcome to the Team!! - Gerald :PP
-welcome - Gwen4
-welcome ngani
 
-Welcome Gwen - Rald :))
+Welcome to the Team!! - Gerald :PP
+
+
+Welcome Gwen - Rald :)
+
+Welcome Gwen - Kian 
