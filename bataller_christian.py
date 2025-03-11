a89c4af1e3eb55578c4d51061835d92825b45874
teves-gwen <tevesgwenn.sch@gmail.com>
2025-03-11 14:35:21 +0800

    Add c

--- bataller_christian.py
+++ bataller_christian.py
@@ -4,11 +4,7 @@ print("Gender: Male")
 print("Email: johndoe@example.com")
 
 Welcome to the Team! - Bataller :+
-<<<<<<< HEAD
-
 Welcome to the team - Gwen
-=======
-
-
 welcome to the Team! - Sario 
->>>>>>> 84bc1661539491ab2422232f73fc616a26d399c1
+
+Welcome to the team - Gonato
